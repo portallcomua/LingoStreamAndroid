10705367415349e90c0d36a7582839b37adfaef6
portallcomua <41564292+portallcomua@users.noreply.github.com>
2026-07-02 00:51:15 +0300

    Create MainActivity.kt

---
+++ android/app/src/main/kotlin/com/lingostream/app/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.lingostream.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+    // Тут нічого не потрібно додавати, просто успадкування від FlutterActivity
+}
